0e82764569c6448d048261708cce8116ed4aabf0
Murilo Portescheller <mport.dev@gmail.com>
2018-01-01 17:57:22 -0200

    Update mascaras.js

--- mascaras.js
+++ mascaras.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	// mascara CPF 
-	$(".cpf-mascara").mask('000.000.000-00', {reverse: true});
+	$(".cpf-mascara").mask('000.000.000-00', {reverse: false});
 	// mascara CNPJ
-	$('.cnpj-mascara').mask('00.000.000/0000-00', {reverse: true});
+	$('.cnpj-mascara').mask('00.000.000/0000-00', {reverse: false});
 	// mascara RG
-	$('.rg-mascara').mask('00.000.000-0', {reverse: true});
+	$('.rg-mascara').mask('00.000.000-0', {reverse: false});
 	// mascara CEP
-	$('.cep-mascara').mask('00000-000', {reverse: true});
+	$('.cep-mascara').mask('00000-000', {reverse: false});
 	// mascara telefone
-	$('.telefone-mascara').mask('(00) 0000-0000', {reverse: true});
+	$('.telefone-mascara').mask('(00) 0000-0000', {reverse: false});
 	// mascara celular
-	$('.celular-mascara').mask('(00) 00000-0000', {reverse: true});
+	$('.celular-mascara').mask('(00) 00000-0000', {reverse: false});
 });
